b5db598c7f57b51cf62446924b22592b792cc732
pascalweiss <pascal@mbp111.local>
2015-10-11 17:22:59 +0200

    Initial Commit

---
+++ DenkmälerBerlinTests/Denkma_lerBerlinTests.swift
@@ -0,0 +1,36 @@
+//
+//  Denkma_lerBerlinTests.swift
+//  DenkmälerBerlinTests
+//
+//  Created by Pascal Weiß on 11.10.15.
+//  Copyright © 2015 HTWBerlin. All rights reserved.
+//
+
+import XCTest
+@testable import Denkma_lerBerlin
+
+class Denkma_lerBerlinTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
