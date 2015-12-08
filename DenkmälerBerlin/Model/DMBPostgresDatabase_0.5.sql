6e9aeb0abbab649c70a69c7f8b867599d3e345d5
pascalweiss <pascal@mbp111.local>
2015-12-08 10:22:02 +0100

    monument-dating hat jetzt eine n:1 Beziehung; monument-address hat jetzt eine n:n Beziehung; monument.name hat jetzt das Format TEXT

---
+++ DenkmälerBerlin/Model/DMBPostgresDatabase_0.5.sql
@@ -0,0 +1,130 @@
+#SET client_min_messages=WARNING;
+
+--- relation-table drops
+DROP TABLE route_rel;
+DROP TABLE district_rel;
+DROP TABLE sub_district_rel;
+DROP TABLE monument_notion_rel;
+DROP TABLE participant_rel;
+DROP TABLE address_rel;
+
+--- entity-table drops
+DROP TABLE history;
+DROP TABLE route;
+DROP TABLE district;
+DROP TABLE sub_district;
+DROP TABLE monument_notion;
+DROP TABLE picture;
+DROP TABLE participant_type;
+DROP TABLE participant;
+DROP TABLE address;
+DROP TABLE monument;
+DROP TABLE dating;
+DROP TABLE type;
+
+
+--- entity-table creation
+CREATE TABLE route (
+	id		SERIAL PRIMARY KEY,
+	name	VARCHAR(40),
+	length	NUMERIC, 
+	descr	TEXT 
+);
+CREATE TABLE type (
+	id		SERIAL PRIMARY KEY,
+	name	VARCHAR(18)
+);
+CREATE TABLE district(
+	id 		SERIAL PRIMARY KEY,
+	name	VARCHAR(30)
+);
+CREATE TABLE sub_district (
+	id 		SERIAL PRIMARY KEY,
+	name	VARCHAR(30)
+);
+CREATE TABLE monument_notion(
+	id 		SERIAL PRIMARY KEY,
+	name	VARCHAR(50)
+);
+CREATE TABLE participant_type(
+	id 			SERIAL PRIMARY KEY,
+	name		VARCHAR(100)
+);
+CREATE TABLE participant(
+	id 			SERIAL PRIMARY KEY,
+	name		VARCHAR(100)
+);
+CREATE TABLE address(
+id              SERIAL PRIMARY KEY,
+lat				NUMERIC,
+long			NUMERIC,
+street			VARCHAR(100),
+nr 				VARCHAR(8)
+);
+CREATE TABLE dating(
+	id 			SERIAL PRIMARY KEY,
+	beginning	DATE,
+	ending 		DATE
+);
+CREATE TABlE monument (
+	id                  SERIAL PRIMARY KEY,
+	name                TEXT,
+	obj_nr              VARCHAR(8),
+	descr               TEXT,
+	type_id             INTEGER REFERENCES type(id),
+	super_monument_id	INTEGER REFERENCES monument(id),
+	link_id             INTEGER REFERENCES monument(id),
+	dating_id			INTEGER REFERENCES dating(id)
+);
+CREATE TABLE picture(
+	id 			SERIAL PRIMARY KEY,
+	url			VARCHAR(200),
+	monument_id INTEGER REFERENCES monument(id)
+);
+
+--- relation-table creation
+CREATE TABLE route_rel(
+	id 			SERIAL PRIMARY KEY,
+	route_id	INTEGER REFERENCES route(id),
+	monument_id	INTEGER REFERENCES monument(id),
+	stage		INTEGER
+);
+CREATE TABLE district_rel(
+	id 			SERIAL PRIMARY KEY,
+	district_id	INTEGER REFERENCES district(id),	
+	monument_id INTEGER REFERENCES monument(id)
+);
+CREATE TABLE sub_district_rel(
+	id 				SERIAL PRIMARY KEY,
+	sub_district_id	INTEGER REFERENCES sub_district(id),
+	monument_id		INTEGER REFERENCES monument(id)
+);
+CREATE TABLE monument_notion_rel(
+	id 					SERIAL PRIMARY KEY,
+	monument_notion_id	INTEGER REFERENCES monument_notion(id),
+	monument_id 		INTEGER REFERENCES monument(id)
+);
+CREATE TABLE participant_rel(
+	id 					SERIAL PRIMARY KEY, 
+	monument_id 		INTEGER REFERENCES monument(id),
+	participant_id		INTEGER REFERENCES participant(id), 
+	participant_type_id	INTEGER REFERENCES participant_type(id)
+);
+CREATE TABLE address_rel(
+	id 					SERIAL PRIMARY KEY,
+	monument_id 		INTEGER REFERENCES monument(id),
+	address_id			INTEGER REFERENCES address(id)
+);
+CREATE TABLE history(
+	search_string		TEXT,
+	time_int_since_1970	NUMERIC
+);
+
+#commit;
+
+
+
+
+
+
+
