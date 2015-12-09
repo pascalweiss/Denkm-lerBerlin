827ef4bb2cb6ab0a44ffbe3b25dca2181789b5a0
pascalweiss <pascal@mbp111.fritz.box>
2015-12-09 22:30:18 +0100

    Added DMBSQLiteFullTextSearch.sql: Erweitert die SQLite Datenbank um Full-Text-Search Funktionen

---
+++ DenkmälerBerlin/Model/DMBSQLiteFullTextSearch.sql
@@ -0,0 +1,23 @@
+--- Integrates full text search into the monument database
+
+CREATE VIRTUAL TABLE monument_search
+USING fts4( id,
+            name,
+            obj_nr,
+            descr,
+            type_id,
+            super_monument_id,
+            link_id,
+            dating_id
+);
+
+INSERT INTO monument_search
+SELECT  id,
+        name,
+        obj_nr,
+        descr,
+        type_id,
+        super_monument_id,
+        link_id,
+        dating_id
+FROM monument;
