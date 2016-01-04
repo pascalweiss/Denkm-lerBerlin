2543a3d44113261a12c9b83be73ad5a613b44d07
Max Oehme <maxoehme@yahoo.de>
2016-01-04 23:28:46 +0100

    UI Search by Name working

--- DenkmälerBerlin/MapViewController.swift
+++ DenkmälerBerlin/MapViewController.swift
@@ -10,18 +10,28 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, UISearchBarDelegate, UISearchDisplayDelegate {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating, UISearchControllerDelegate{
     
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var searchMap: UISearchBar!
-    
     var clManager: CLLocationManager!
-    var filteredData: [String]!
+    
+    let sectionNames = ["Name"]
+    
+    var monumentString: [String] = []
+    var monuments: [DMBMonument] = []
+    
+    var searchHistory: [String] = ["Schloss", "Kirche"]
+    
+    var filteredData = Array(count: 5, repeatedValue: Array(count: 0, repeatedValue: String()))
+    var searchController: UISearchController!
+    
+    var searchResultsTableView = UITableViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        navigationController?.setNavigationBarHidden(navigationController?.navigationBarHidden == false, animated: false)
+
+        DMBModel.sharedInstance
+        monuments = DMBModel.sharedInstance.getAllMonuments()
         
         clManager = initMapLocationManager()
         let anno = DenkmalMapAnnotation(latitude: 53.800337, longitude: 12.178451)
@@ -31,14 +41,11 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         
         mapView.addAnnotation(anno)
         
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        navigationController?.setNavigationBarHidden(navigationController?.navigationBarHidden == false, animated: true)
-    }
-    
-    override func viewWillDisappear(animated: Bool) {
-        navigationController?.setNavigationBarHidden(navigationController?.navigationBarHidden == false, animated: true)
+        setupSearchController()
+        setupSearchResultsTable()
+        
+        
+        
     }
     
     func initMapLocationManager() -> CLLocationManager {
@@ -95,19 +102,128 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         }
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        self.searchMap.endEditing(true)
+    // MARK: Search
+    
+    func setupSearchController(){
+        
+        //filteredData = data
+        
+        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.delegate = self
+        
+        searchController.dimsBackgroundDuringPresentation = false
+        
+        // Serach Bar in Navigation
+        searchController.searchBar.sizeToFit()
+        navigationItem.titleView = searchController.searchBar
+        searchController.hidesNavigationBarDuringPresentation = false
+        
+        // Sets this view controller as presenting view controller for the search interface
+        definesPresentationContext = true
+        
     }
     
-    // MARK: Search
+    func setupSearchResultsTable(){
+        searchResultsTableView.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "SearchTabelCell")
+        
+        searchResultsTableView.tableView.dataSource = self
+        searchResultsTableView.tableView.delegate = self
+        
+        //searchResultsTableView.automaticallyAdjustsScrollViewInsets = true
+
+        configSearchResultsTableView()
+        
+        
+        self.view.addSubview(searchResultsTableView.tableView)
+    }
+    
+    func configSearchResultsTableView(){
+        // Hide Table at Map
+        searchResultsTableView.tableView.hidden = true
+        
+        // Set Size and Position of Map
+        let viewFrame = self.view.frame
+        let x = self.navigationController?.navigationBar.frame.origin.x
+        let y = (self.navigationController?.navigationBar.frame.origin.y)! + (self.navigationController?.navigationBar.frame.height)!
+        let originPoint = CGPoint(x: x!, y: y)
+        let searchResultsTableViewRect = CGRect(origin: originPoint, size: viewFrame.size)
+        searchResultsTableView.tableView.frame = searchResultsTableViewRect
+        
+        // Color and Transparency
+        searchResultsTableView.tableView.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.7)
+    }
+    
+    func willPresentSearchController(searchController: UISearchController) {
+        searchResultsTableView.tableView.hidden = false
+    }
+    
+    func willDismissSearchController(searchController: UISearchController) {
+        searchResultsTableView.tableView.hidden = true
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if (section == 0 || filteredData[section].isEmpty) {
+            return nil
+        }
+        return sectionNames[section - 1]
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        // Text Color for Header
+        let header = view as! UITableViewHeaderFooterView
+        header.textLabel?.textColor = UIColor.whiteColor()
+    }
+
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("TableCell")! as UITableViewCell
-        cell.textLabel?.text = filteredData[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier("SearchTabelCell")! as UITableViewCell
+        
+        switch(indexPath.section){
+        case 0: cell.textLabel?.text = filteredData[0][indexPath.row]
+        case 1: cell.textLabel?.text = filteredData[1][indexPath.row]
+        default: break
+        }
+        
+        // Color and Transparency Settings
+        cell.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.5)
+        cell.textLabel?.textColor = UIColor.whiteColor()
         return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        switch(section){
+        case 0: return filteredData[0].count
+        case 1: return filteredData[1].count
+        default: return 0
+        }
+        
+        
     }
     
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        let searchText = searchController.searchBar.text
+        
+        for i in 0..<filteredData.count {
+            filteredData[i].removeAll()
+        }
+        
+        if searchText?.isEmpty == false {
+            let filteredMonuments = monuments.filter(){
+                return $0.getName()!.rangeOfString(searchText!, options: .CaseInsensitiveSearch) != nil
+            }
+            
+            for i in 0..<filteredMonuments.count {
+                filteredData[1].append(filteredMonuments[i].getName()!)
+            }
+        } else {
+            filteredData[0] = searchHistory
+        }
+        
+        searchResultsTableView.tableView.reloadData()
+    }
 }
