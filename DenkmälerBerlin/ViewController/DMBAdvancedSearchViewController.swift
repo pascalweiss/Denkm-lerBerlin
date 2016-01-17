f685d41f528652c518ba61391aa01176a834fe49
Sebastian Fuchs <sebastian.fuchs@helia.com>
2016-01-17 14:16:30 +0100

    Einbau des Range Sliders in der erweiterten Suche

--- DenkmälerBerlin/ViewController/DMBAdvancedSearchViewController.swift
+++ DenkmälerBerlin/ViewController/DMBAdvancedSearchViewController.swift
@@ -18,11 +18,43 @@ class DMBAdvancedSearchViewController: UITableViewController {
     
     var monumentType: [(type: String,on: Bool)] = [("Baudenkmal", true), ("Garten- /Parkdenkmal", true), ("Bodendenkmal", true)]
     
+    // MARK: Elemente fuer Range Slider
+    let customRangeSlider = RangeSlider(frame: CGRectZero)
+    let labelLowerValue = UILabel()
+    let labelUpperValue = UILabel()
+    
     // MARK: Life-Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.registerClass(DMBAdvSearchTableViewCell.self, forCellReuseIdentifier: NSStringFromClass(DMBAdvSearchTableViewCell))
+        
+        // Range Slider inklusive Wertanzeige in View einbinden
+        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(customRangeSlider)
+        customRangeSlider.addTarget(self, action: "rangeSliderValueChanged:", forControlEvents: .ValueChanged)
+        
+        labelLowerValue.text = "\(Int(customRangeSlider.lowerValue))"
+        view.addSubview(labelLowerValue)
+        
+        labelUpperValue.text = "\(Int(customRangeSlider.upperValue))"
+        view.addSubview(labelUpperValue)
+        
+    }
+    
+    // MARK: Range Slider positionieren
+    override func viewDidLayoutSubviews() {
+        let margin: CGFloat = 20.0
+        let width = view.bounds.width - 2.0 * margin
+        let height: CGFloat = 31.0;
+        
+        customRangeSlider.frame = CGRect(x: margin, y: margin + topLayoutGuide.length + 100, width: width, height: height)
+        
+        labelLowerValue.frame = CGRect(x: margin, y: margin + topLayoutGuide.length + 100 + height, width: 200, height: height)
+        labelLowerValue.textAlignment = NSTextAlignment.Left;
+        
+        labelUpperValue.frame = CGRect(x: view.frame.size.width - margin - 200, y: margin + topLayoutGuide.length + 100 + height, width: 200, height: height)
+        labelUpperValue.textAlignment = NSTextAlignment.Right;
         
     }
 
@@ -72,7 +104,18 @@ class DMBAdvancedSearchViewController: UITableViewController {
         return cell
     }
     
+    // MARK: Kann die raus???
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
-
+    // MARK: Veraenderte Position Schieberegler des Range Sliders holen
+    func rangeSliderValueChanged(rangeSlider: RangeSlider) {
+        print("Range slider value changed: (\(rangeSlider.lowerValue) , \(rangeSlider.upperValue))")
+        
+        labelLowerValue.text = "\(Int(rangeSlider.lowerValue))"
+        labelUpperValue.text = "\(Int(rangeSlider.upperValue))"
+    }
 
 }
